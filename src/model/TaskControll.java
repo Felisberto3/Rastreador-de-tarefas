ab983c9ac3357e7fae15bcb47c4bc3d1afd382c3
Felisberto Alberto <felisbertoalberto183@gmail.com>
2025-02-18 00:50:45 +0100

    funcionalidades de task-cli concruidas

--- src/model/TaskControll.java
+++ src/model/TaskControll.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
 import com.google.gson.JsonIOException;
@@ -32,10 +33,12 @@ public class TaskControll {
 
         } else if (method.equals("delete")) {
             int id = Integer.parseInt(args[2]);
+
             TaskModel.delete(id);
 
         } else if (method.equals("update")) {
             int id = Integer.parseInt(args[2]);
+
             String value = args[3].trim();
 
             if (value.length() > 2)
@@ -43,12 +46,44 @@ public class TaskControll {
 
         } else if (method.equals("mark-in-progress") || method.equals("mark-done")) {
             int id = Integer.parseInt(args[2]);
+
             String value = method.equals("mark-in-progress") ? "In-progress" : "done";
 
             if (value.length() > 2)
                 TaskModel.update(id, "status", value);
+
+        } else if (method.equals("list")) {
+
+            if (args.length <= 2) {
+                String condition = "all";
+                list(condition);
+
+            } else if (args[2].equals("done")) {
+                String condition = "done";
+                list(condition);
+
+            } else if (args[2].equals("in-progress")) {
+                String condition = "in-progress";
+                list(condition);
+            }
         }
 
+    }
+
+    private void list(String condition) throws JsonIOException, IOException {
+        List<Task> tasks = TaskModel.allTasks();
+        if (condition.equals("all")) {
+            for (Task task : tasks) {
+                System.out.println(task);
+            }
+            return;
+        }
+
+        for (Task task : tasks) {
+            if (task.getStatus().equals(condition)) {
+                System.out.println(task);
+            }
+        }
     }
 
 }
